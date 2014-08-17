52fbdfb4def4b7305c88a1fb60aa4c4a2ddd7018
Niall Mullally <niall.mullally@gmail.com>
2014-08-17 09:06:12 +0100

    Fix installer

--- Install.php
+++ Install.php
@@ -8,6 +8,8 @@ class Sidane_Threadmarks_Install
       return;
     }
 
+    $db = XenForo_Application::get('db');
+    
     $db->query("
       CREATE TABLE IF NOT EXISTS threadmarks (
         threadmark_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -18,7 +20,6 @@ class Sidane_Threadmarks_Install
       ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci
     ");
 
-    $db = XenForo_Application::get('db');
     $db->query("ALTER TABLE xf_thread ADD COLUMN has_threadmarks INT UNSIGNED DEFAULT 0 NOT NULL AFTER prefix_id");
   }
 
