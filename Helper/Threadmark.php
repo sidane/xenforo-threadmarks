2c19e497a063739e9b34bca8ff503f4ec03f8f45
Niall Mullally <niall.mullally@gmail.com>
2014-11-26 23:02:10 +0000

    Ensure there are threadmarks before rendering threadmarker

--- Helper/Threadmark.php
+++ Helper/Threadmark.php
@@ -5,7 +5,7 @@ class Sidane_Threadmarks_Helper_Threadmark extends XenForo_Template_Helper_Core
 
   public static function renderThreadmarkFlag($postId, $threadmarks)
   {
-    if (array_key_exists($postId, $threadmarks)) {
+    if (count($threadmarks) > 0 && array_key_exists($postId, $threadmarks)) {
       $threadmark = $threadmarks[$postId];
       $threadmarkPhrase = new XenForo_Phrase('threadmark');
       return "<span class='threadmarker' id='post-{$postId}'><strong>{$threadmarkPhrase}:</strong> {$threadmark['label']}</span>";
