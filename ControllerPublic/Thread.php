2af3d80b1fcff15d2c756db34dc8eb54887e74da
Niall Mullally <niall.mullally@gmail.com>
2014-11-30 16:57:44 +0000

    Only show most recent threadmarks

--- ControllerPublic/Thread.php
+++ ControllerPublic/Thread.php
@@ -10,8 +10,22 @@ class Sidane_Threadmarks_ControllerPublic_Thread extends XFCP_Sidane_Threadmarks
 
     if ($thread['has_threadmarks']) {
       $threadmarksModel = $this->getModelFromCache('Sidane_Threadmarks_Model_Threadmarks');
+      $menuLimit = intval(XenForo_Application::get('options')->threadmarksLimit);
+
       $threadmarks = $threadmarksModel->getByThreadId($thread['thread_id']);
+      $totalThreadmarks = count($threadmarks);
+
+      if ($totalThreadmarks == 0) {
+        return $parent;
+      }
+
+      if ($totalThreadmarks > $menuLimit) {
+        $threadmarks = array_slice($threadmarks, $totalThreadmarks - $menuLimit, null, true);
+        $parent->params['thread']['more_threadmarks'] = true;
+      }
+
       $parent->params['thread']['threadmarks'] = $threadmarks;
+      $parent->params['thread']['total_threadmarks'] = $totalThreadmarks;
 
       $parent->params['thread']['threadmarks_post_ids'] = array_map(function($threadmark) {
         return $threadmark['post_id'];
