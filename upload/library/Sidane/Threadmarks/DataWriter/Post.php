7ba491f6496ec344f3b0c2e3f9fb9e7219fa8c8c
Xon <tazipper1@hotmail.com>
2015-01-10 01:24:06 +0800

    Missing parent::_messagePostDelete

--- upload/library/Sidane/Threadmarks/DataWriter/Post.php
+++ upload/library/Sidane/Threadmarks/DataWriter/Post.php
@@ -5,11 +5,11 @@ class Sidane_Threadmarks_DataWriter_Post extends XFCP_Sidane_Threadmarks_DataWri
   protected function _messagePostSave()
   {
     parent::_messagePostSave();
-                        
+
     if ($this->isChanged('message_state'))
     {
       $threadmark = $this->_getThreadMarkModel()->getByThreadIdAndPostId($this->get('thread_id'), $this->get('post_id'));
-                
+
       if (!empty($threadmark))
       {
         if ($this->get('message_state') == 'visible')
@@ -21,19 +21,21 @@ class Sidane_Threadmarks_DataWriter_Post extends XFCP_Sidane_Threadmarks_DataWri
           $this->_getThreadMarkModel()->modifyThreadMarkCount($this->get('thread_id'), -1);
         }
       }
-    }        
+    }
   }
-    
+
   protected function _messagePostDelete()
   {
+    parent::_messagePostDelete();
+
     $threadmark = $this->_getThreadMarkModel()->getByThreadIdAndPostId($this->get('thread_id'), $this->get('post_id'));
     if (!empty($threadmark))
     {
       $decrementCount = ($this->isUpdate() && $this->getExisting('message_state') == 'visible');
       $this->_getThreadMarkModel()->deleteThreadMark($threadmark['threadmark_id'], $decrementCount);
     }
-  }    
-    
+  }
+
   protected function _getThreadMarkModel()
   {
     return $this->getModelFromCache('Sidane_Threadmarks_Model_Threadmarks');
