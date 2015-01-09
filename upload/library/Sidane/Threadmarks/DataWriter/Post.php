9080767c32edd50cb9cb7cf14fafc2ad1edca5f9
Xon <tazipper1@hotmail.com>
2015-01-09 11:28:58 +0800

    Convert tabs -> 2 space characters

--- upload/library/Sidane/Threadmarks/DataWriter/Post.php
+++ upload/library/Sidane/Threadmarks/DataWriter/Post.php
@@ -2,40 +2,40 @@
 
 class Sidane_Threadmarks_DataWriter_Post extends XFCP_Sidane_Threadmarks_DataWriter_Post
 {
-	protected function _messagePostSave()
-	{
-        parent::_messagePostSave();
+  protected function _messagePostSave()
+  {
+    parent::_messagePostSave();
                         
-		if ($this->isChanged('message_state'))
-		{
-            $threadmark = $this->_getThreadMarkModel()->getByThreadIdAndPostId($this->get('thread_id'), $this->get('post_id'));
-                      
-            if (!empty($threadmark))
-            {
-                if ($this->get('message_state') == 'visible')
-                {
-                    $this->_getThreadMarkModel()->modifyThreadMarkCount($this->get('thread_id'), 1);
-                }
-                else if ($this->isUpdate() && $this->getExisting('message_state') == 'visible')
-                {
-                    $this->_getThreadMarkModel()->modifyThreadMarkCount($this->get('thread_id'), -1);
-                }
-            }
-		}        
-    }
-    
-	protected function _messagePostDelete()
-	{
-        $threadmark = $this->_getThreadMarkModel()->getByThreadIdAndPostId($this->get('thread_id'), $this->get('post_id'));
-        if (!empty($threadmark))
-        {
-            $decrementCount = ($this->isUpdate() && $this->getExisting('message_state') == 'visible');
-            $this->_getThreadMarkModel()->deleteThreadMark($threadmark['threadmark_id'], $decrementCount);
-        }
-	}    
-    
-    protected function _getThreadMarkModel()
+    if ($this->isChanged('message_state'))
     {
-        return $this->getModelFromCache('Sidane_Threadmarks_Model_Threadmarks');
+      $threadmark = $this->_getThreadMarkModel()->getByThreadIdAndPostId($this->get('thread_id'), $this->get('post_id'));
+                
+      if (!empty($threadmark))
+      {
+        if ($this->get('message_state') == 'visible')
+        {
+          $this->_getThreadMarkModel()->modifyThreadMarkCount($this->get('thread_id'), 1);
+        }
+        else if ($this->isUpdate() && $this->getExisting('message_state') == 'visible')
+        {
+          $this->_getThreadMarkModel()->modifyThreadMarkCount($this->get('thread_id'), -1);
+        }
+      }
+    }        
+  }
+    
+  protected function _messagePostDelete()
+  {
+    $threadmark = $this->_getThreadMarkModel()->getByThreadIdAndPostId($this->get('thread_id'), $this->get('post_id'));
+    if (!empty($threadmark))
+    {
+      $decrementCount = ($this->isUpdate() && $this->getExisting('message_state') == 'visible');
+      $this->_getThreadMarkModel()->deleteThreadMark($threadmark['threadmark_id'], $decrementCount);
     }
+  }    
+    
+  protected function _getThreadMarkModel()
+  {
+    return $this->getModelFromCache('Sidane_Threadmarks_Model_Threadmarks');
+  }
 }
