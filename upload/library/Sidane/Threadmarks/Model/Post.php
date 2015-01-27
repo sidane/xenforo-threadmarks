d4f5d00984890f647c49e0a998ee5424b1b8e74e
Xon <tazipper1@hotmail.com>
2015-01-28 00:44:56 +0800

    Missing populating the view permission check in the post.

--- upload/library/Sidane/Threadmarks/Model/Post.php
+++ upload/library/Sidane/Threadmarks/Model/Post.php
@@ -12,6 +12,7 @@ class Sidane_Threadmarks_Model_Post extends XFCP_Sidane_Threadmarks_Model_Post
     $post['canAddThreadmarks'] = $threadmarkmodel->canAddThreadmark($post, $thread, $forum, $null, $nodePermissions, $viewingUser);
     $post['canEditThreadmarks'] = $threadmarkmodel->canEditThreadmark($post, $thread, $forum, $null, $nodePermissions, $viewingUser);
     $post['canDeleteThreadmarks'] = $threadmarkmodel->canDeleteThreadmark($post, $thread, $forum, $null, $nodePermissions, $viewingUser);
+    $post['canViewThreadmarks'] = $threadmarkmodel->canViewThreadmark($post, $thread, $forum, $null, $nodePermissions, $viewingUser);
 
     return $post;
   }
