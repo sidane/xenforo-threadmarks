3350ddaaddc3468fb602b6c3eb9a31ce1b354e81
Xon <tazipper1@hotmail.com>
2015-01-27 22:44:09 +0800

    Default permissions setup.

--- upload/library/Sidane/Threadmarks/Install.php
+++ upload/library/Sidane/Threadmarks/Install.php
@@ -2,6 +2,8 @@
 
 class Sidane_Threadmarks_Install
 {
+  const default_menu_limit = 8;
+
   public static function install($existingAddOn, $addOnData)
   {
     $version = isset($existingAddOn['version_id']) ? $existingAddOn['version_id'] : 0;
@@ -38,13 +40,49 @@ class Sidane_Threadmarks_Install
     if ($version < 2)
     {
       if (!$db->fetchRow("SHOW COLUMNS FROM xf_thread WHERE Field = ?", 'threadmark_count'))
-      {    
+      {
         $db->query("ALTER TABLE xf_thread ADD COLUMN threadmark_count INT UNSIGNED DEFAULT 0 NOT NULL");
       }
       if (!$db->fetchRow("SHOW INDEX FROM threadmarks WHERE Key_name = ?", 'thread_post_id'))
       {
           $db->query("ALTER TABLE threadmarks add unique index thread_post_id (`thread_id`,`post_id`)");
       }
+
+      $db->query("insert ignore into xf_permission_entry_content (content_type, content_id, user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
+        select distinct content_type, content_id, user_group_id, user_id, convert(permission_group_id using utf8), 'sidane_tm_manage', permission_value, permission_value_int
+        from xf_permission_entry_content
+        where permission_group_id = 'forum' and permission_id in ('warn','editAnyPost','deleteAnyPost')
+        ");
+
+      $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
+        select distinct user_group_id, user_id, convert(permission_group_id using utf8), 'sidane_tm_add', permission_value, permission_value_int
+        from xf_permission_entry
+        where permission_group_id = 'forum' and permission_id in ('postReply')
+        ");
+
+      $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
+        select distinct user_group_id, user_id, convert(permission_group_id using utf8), 'sidane_tm_delete', permission_value, permission_value_int
+        from xf_permission_entry
+        where permission_group_id = 'forum' and permission_id in ('deleteOwnPost')
+        ");
+
+      $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
+        select distinct user_group_id, user_id, convert(permission_group_id using utf8), 'sidane_tm_edit', permission_value, permission_value_int
+        from xf_permission_entry
+        where permission_group_id = 'forum' and permission_id in ('editOwnPost')
+        ");
+
+      $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
+        select distinct user_group_id, user_id, convert(permission_group_id using utf8), 'sidane_tm_menu_limit', ".self::default_menu_limit.", permission_value_int
+        from xf_permission_entry
+        where permission_group_id = 'forum' and  permission_id in ('viewContent')
+        ");
+
+      $db->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
+        select distinct user_group_id, user_id, convert(permission_group_id using utf8), 'sidane_tm_view', permission_value, permission_value_int
+        from xf_permission_entry
+        where permission_group_id = 'forum' and  permission_id in ('viewContent')
+        ");
     }
 
     XenForo_Application::defer('Sidane_Threadmarks_Deferred_Cache', array(), null, true);
@@ -56,7 +94,21 @@ class Sidane_Threadmarks_Install
     if ($db->fetchRow("SHOW COLUMNS FROM xf_thread WHERE Field = ?", 'has_threadmarks'))
     {
       $db->query("ALTER TABLE xf_thread DROP COLUMN threadmark_count");
-    }  
+    }
     $db->query("DROP TABLE IF EXISTS threadmarks");
+
+    $db->delete('xf_permission_entry', "permission_id = 'sidane_tm_manage'");
+    $db->delete('xf_permission_entry', "permission_id = 'sidane_tm_add'");
+    $db->delete('xf_permission_entry', "permission_id = 'sidane_tm_delete'");
+    $db->delete('xf_permission_entry', "permission_id = 'sidane_tm_edit'");
+    $db->delete('xf_permission_entry', "permission_id = 'sidane_tm_menu_limit'");
+    $db->delete('xf_permission_entry', "permission_id = 'sidane_tm_view'");
+
+    $db->delete('xf_permission_entry_content', "permission_id = 'sidane_tm_manage'");
+    $db->delete('xf_permission_entry_content', "permission_id = 'sidane_tm_add'");
+    $db->delete('xf_permission_entry_content', "permission_id = 'sidane_tm_delete'");
+    $db->delete('xf_permission_entry_content', "permission_id = 'sidane_tm_edit'");
+    $db->delete('xf_permission_entry_content', "permission_id = 'sidane_tm_menu_limit'");
+    $db->delete('xf_permission_entry_content', "permission_id = 'sidane_tm_view'");
   }
 }
