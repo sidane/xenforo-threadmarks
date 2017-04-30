ff8519dec32e9fc6c720d97db696e361c8c23bfd
Jeremy P <jrahmy+git@gmail.com>
2017-04-30 02:55:38 -0700

    Update ThreadmarksLoadTree view execution order and fix code formatting

--- upload/library/Sidane/Threadmarks/XenForo/ViewPublic/Thread/ThreadmarksLoadTree.php
+++ upload/library/Sidane/Threadmarks/XenForo/ViewPublic/Thread/ThreadmarksLoadTree.php
@@ -2,10 +2,10 @@
 
 class Sidane_Threadmarks_XenForo_ViewPublic_Thread_ThreadmarksLoadTree extends XFCP_Sidane_Threadmarks_XenForo_ViewPublic_Thread_ThreadmarksLoadTree
 {
-    public function renderJson()
-    {
-        return XenForo_ViewRenderer_Json::jsonEncodeForOutput(array(
-            'tree' => $this->_params['tree']
-        ));
-    }
+  public function renderJson()
+  {
+    return XenForo_ViewRenderer_Json::jsonEncodeForOutput(array(
+      'tree' => $this->_params['tree']
+    ));
+  }
 }
